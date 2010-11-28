0704eaae2c603da2fde1a5cbc023ae2095bd82b0
Peter Bengtsson <peter@fry-it.com>
2010-11-28 23:50:14 +0000

    modified .gitignore, setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-from setuptools import setup, find_packages
+#from setuptools import setup, find_packages
+from distutils.core import setup
 import sys, os
 
-version = '0.2'
-long_description = open('README.md').read().strip() + "\n\n"
+version = '0.2.2'
+here = os.path.dirname(__file__)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 def md2stx(s):
     import re
     s = re.sub(':\n(\s{8,10})', r'::\n\1', s)
     return s
-long_description = md2stx(long_description)
-            
 
 setup(name='python-donecal',
       version=version,
       description="Python interface for the donecal.com restful HTTP API",
-      long_description=long_description,
+      long_description=md2stx(read('README.md')),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='donecal rest api donecal.com',
       author='Peter Bengtsson',
       author_email='mail@peterbe.com',
       url='http://donecal.com/help/API#Python',
       license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=[
+        'donecal',
+      ],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
